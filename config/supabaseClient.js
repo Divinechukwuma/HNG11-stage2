15f645f8582a6d3e8388e87e91dd8c61e6ae4034
divinechukwuma <divinechukwuma982@gmail.com>
2024-07-06 11:43:28 +0100

    connecting my database

---
+++ config/supabaseClient.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = process.env.NODE_SUPABASE_URL
+const supabaseKey = process.env.E_SUPABASE_API_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default  supabase
